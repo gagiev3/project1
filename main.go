ba7f0d8ddabda755488aa099dae66fd218829d5c
gagiev3 <dmitriy2015scorpion@gmail.com>
2024-02-09 14:43:22 +0200

    if topic3.5

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	a := sqrt(2)
+	b := sqrt(-4)
+	fmt.Println(a, b)
+}
