3672e959a41f3feae3dd999dca56ba263824b56b
gagiev3 <dmitriy2015scorpion@gmail.com>
2024-02-19 11:16:56 +0200

    working with slices

---
+++ domain/human.go
@@ -0,0 +1,8 @@
+package domain
+
+type Human struct {
+	Id      uint64
+	Name    string
+	Age     uint8
+	Country string
+}
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
-	}
-	return fmt.Sprint(math.Sqrt(x))
-}
+import "fmt"
 
 func main() {
-	a := sqrt(2)
-	b := sqrt(-4)
-	fmt.Println(a, b)
+	names := []string{"Vlad", "Nastya", "Stas", "Maksim", "Artem", "Dima"}
+
+	names = append(names, "Vika")
+	names = append(names, "Vitaliy")
+
+	fmt.Println(names)
+
+	var (
+		parni   []int
+		neparni []int
+	)
+
+	for i := 0; i <= 20; i++ {
+		if i%2 == 0 {
+			parni = append(parni, i)
+		}
+		if i%2 != 0 {
+			neparni = append(neparni, i)
+		}
+	}
+	fmt.Println(parni)
+	fmt.Println(neparni)
 }
